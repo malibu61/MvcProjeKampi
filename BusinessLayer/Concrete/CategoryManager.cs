3ee5db74604d5ba89c50c7adb110a6723f5fa21e
Muhammet Ali Tanrıkulu <muhammedd45@icloud.com>
2024-12-05 13:48:07 +0300

    Add project files.

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,33 @@
+﻿using DataAccessLayer.Concrete;
+using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public interface CategoryManager
+    {
+        GenericRepository<Category> repo = new GenericRepository<Category>();
+
+        public List<Category> GetAllBL()
+        {
+            return repo.List();
+        }
+
+        public void CategoryAddBL(Category category)
+        {
+            if (category.CategoryName == "" || category.CategoryName.Length <= 3 || category.CategoryDescription == "" || category.CategoryName.Length >= 51)
+            {
+                //Hata Mesajı
+            }
+            else
+            {
+                repo.Insert(category);
+            }
+        }
+    }
+}
---
+++ DataAccessLayer/Concrete/Repositories/CategoryRepository.cs
@@ -0,0 +1,39 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        Context context = new Context();
+        DbSet<Category> _object;
+
+        public void Delete(Category category)
+        {
+            _object.Remove(category);
+            context.SaveChanges();
+        }
+
+        public void Insert(Category category)
+        {
+            _object.Add(category);
+            context.SaveChanges();
+        }
+
+        public List<Category> List()
+        {
+            return _object.ToList();
+        }
+
+        public void Update(Category category)
+        {
+            context.SaveChanges();
+        }
+    }
+}
